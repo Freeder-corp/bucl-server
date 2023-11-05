658ccc752d1ae11fd4ebc42f07b9ccbd6175793a
Changmin Kim <ckd29672@naver.com>
2023-11-06 02:42:59 +0900

    테스트: git hook: pre-commit

--- src/main/java/com/freeder/buclserver/BuclServerApplication.java
+++ src/main/java/com/freeder/buclserver/BuclServerApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BuclServerApplication {
+	private String ai;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BuclServerApplication.class, args);
 	}
