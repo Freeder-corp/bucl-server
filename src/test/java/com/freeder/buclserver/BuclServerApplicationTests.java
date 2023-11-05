6919049288ca1a4fc122c61e677b8f1333891128
Changmin Kim <ckd29672@naver.com>
2023-11-05 19:39:39 +0900

    initialize bucl project

---
+++ src/test/java/com/freeder/buclserver/BuclServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeder.buclserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuclServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
