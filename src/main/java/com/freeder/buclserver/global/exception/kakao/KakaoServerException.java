b591fbbb1ea9d14f4bd30bd2a20a2e3125a53632
sujeong11 <qalzm7351@gmail.com>
2023-11-22 00:11:17 +0900

    Refactor: errorCode를 HttpStatus의 값을 사용하도록 변경

--- src/main/java/com/freeder/buclserver/global/exception/kakao/KakaoServerException.java
+++ src/main/java/com/freeder/buclserver/global/exception/kakao/KakaoServerException.java
@@ -1,12 +1,12 @@
 package com.freeder.buclserver.global.exception.kakao;
 
-import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.*;
 
 import com.freeder.buclserver.global.exception.BaseException;
 
 public class KakaoServerException extends BaseException {
 
 	public KakaoServerException() {
-		super(HttpStatus.INTERNAL_SERVER_ERROR, 500, "카카오 서버와 통신 중 에러가 발생했습니다.");
+		super(INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR.value(), "카카오 서버와 통신 중 에러가 발생했습니다.");
 	}
 }
