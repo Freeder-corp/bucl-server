baf84c99f6177aec7292e120a89c68d895eff1cf
sujeong11 <qalzm7351@gmail.com>
2023-11-21 13:13:53 +0900

    rename: 패키지명 oauth2 -> auth로 변경
    - refresh 토큰 기능이 추가되면서 더 넓은 의미인 auth로 패키지 이름을 변경합니다.

--- src/main/java/com/freeder/buclserver/app/auth/service/JwtTokenService.java
+++ src/main/java/com/freeder/buclserver/app/auth/service/JwtTokenService.java
@@ -1,9 +1,9 @@
-package com.freeder.buclserver.app.oauth2.service;
+package com.freeder.buclserver.app.auth.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.freeder.buclserver.app.oauth2.dto.response.TokenResponse;
+import com.freeder.buclserver.app.auth.dto.response.TokenResponse;
 import com.freeder.buclserver.core.security.JwtTokenProvider;
 import com.freeder.buclserver.domain.user.entity.User;
 import com.freeder.buclserver.domain.user.repository.UserRepository;
