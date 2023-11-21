fbb34c6c61c2e9a70e547dee2403ac502befb678
sujeong11 <qalzm7351@gmail.com>
2023-11-22 00:11:17 +0900

    refactor: errorCode를 HttpStatus의 값을 사용하도록 변경

--- src/main/java/com/freeder/buclserver/global/exception/auth/RefreshTokenNotFoundException.java
+++ src/main/java/com/freeder/buclserver/global/exception/auth/RefreshTokenNotFoundException.java
@@ -1,12 +1,12 @@
 package com.freeder.buclserver.global.exception.auth;
 
-import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.*;
 
 import com.freeder.buclserver.global.exception.BaseException;
 
 public class RefreshTokenNotFoundException extends BaseException {
 
 	public RefreshTokenNotFoundException() {
-		super(HttpStatus.NOT_FOUND, 404, "해당 refresh 토큰을 가진 사용자가 없습니다.");
+		super(NOT_FOUND, NOT_FOUND.value(), "해당 refresh 토큰을 가진 사용자가 없습니다.");
 	}
 }
