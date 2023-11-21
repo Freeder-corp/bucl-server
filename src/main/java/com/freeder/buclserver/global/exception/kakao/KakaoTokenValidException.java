e2db78b0257e533a3a5c3317e1857183170427b4
sujeong11 <qalzm7351@gmail.com>
2023-11-22 00:11:17 +0900

    refactor: errorCode를 HttpStatus의 값을 사용하도록 변경

--- src/main/java/com/freeder/buclserver/global/exception/kakao/KakaoTokenValidException.java
+++ src/main/java/com/freeder/buclserver/global/exception/kakao/KakaoTokenValidException.java
@@ -1,12 +1,12 @@
 package com.freeder.buclserver.global.exception.kakao;
 
-import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.*;
 
 import com.freeder.buclserver.global.exception.BaseException;
 
 public class KakaoTokenValidException extends BaseException {
 
 	public KakaoTokenValidException() {
-		super(HttpStatus.UNAUTHORIZED, 401, "유효하지 않는 KAKAO 토큰입니다.");
+		super(UNAUTHORIZED, UNAUTHORIZED.value(), "유효하지 않는 KAKAO 토큰입니다.");
 	}
 }
