4421f47a49c7dd3fd8491b53710f551da8ba03be
Changmin Kim <ckd29672@naver.com>
2023-11-23 23:45:29 +0900

    feat: 에러 관련 핸들러 추가
    - handleIoException : IOException 처리 핸들러
    - handleIamportResponseException : 아임포트 결제 api 오류 처리 핸들러

--- src/main/java/com/freeder/buclserver/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/freeder/buclserver/global/exception/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.freeder.buclserver.global.exception;
 
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.freeder.buclserver.global.response.ErrorResponse;
+import com.siot.IamportRestClient.exception.IamportResponseException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -12,5 +16,16 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<?> handleRestApiException(BaseException error) {
 		return new ResponseEntity<>(new ErrorResponse(error.getHttpStatus(), error.getErrorMessage()),
 			error.getHttpStatus());
+	}
+
+	@ExceptionHandler(IOException.class)
+	public ResponseEntity<?> handleIoException(IOException error) {
+		return new ResponseEntity<>(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "").getStatusCode());
+	}
+
+	@ExceptionHandler(IamportResponseException.class)
+	public ResponseEntity<?> handleIamportResponseException(IamportResponseException error) {
+		return new ResponseEntity<>(
+			new ErrorResponse(HttpStatus.valueOf(error.getHttpStatusCode()), "결제 api에서 오류가 발생했습니다.").getStatusCode());
 	}
 }
