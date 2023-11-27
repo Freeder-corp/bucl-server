0cf2eee1f6cb85313e323801219ecf34139e60bd
sujeong11 <qalzm7351@gmail.com>
2023-11-27 22:18:11 +0900

    fix: 로그아웃된 사용자는 탈퇴가 되지 않도록 로직 추가

--- src/main/java/com/freeder/buclserver/app/user/UserService.java
+++ src/main/java/com/freeder/buclserver/app/user/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.freeder.buclserver.domain.user.dto.UserDto;
 import com.freeder.buclserver.domain.user.entity.User;
 import com.freeder.buclserver.domain.user.repository.UserRepository;
+import com.freeder.buclserver.global.exception.auth.WithdrawalBadRequestException;
 import com.freeder.buclserver.global.exception.user.UserIdNotFoundException;
 
 import lombok.RequiredArgsConstructor;
@@ -42,6 +43,10 @@ public class UserService {
 	public void withdrawal(Long userId) {
 		User user = userRepository.findById(userId)
 			.orElseThrow(() -> new UserIdNotFoundException(userId));
+
+		if (user.getRefreshToken() == null) {
+			throw new WithdrawalBadRequestException();
+		}
 
 		user.withdrawal();
 	}
