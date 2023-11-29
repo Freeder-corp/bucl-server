bee157468bae8c4b8c6adabbb9e9d835210a6a21
sujeong11 <qalzm7351@gmail.com>
2023-11-29 21:39:20 +0900

    feat: 자신이 생성한 홍보 링크 리스트 조회 기능 구현

--- src/main/java/com/freeder/buclserver/app/user/controller/UserController.java
+++ src/main/java/com/freeder/buclserver/app/user/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.freeder.buclserver.app.user.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.freeder.buclserver.app.affiliates.service.AffiliateService;
 import com.freeder.buclserver.app.user.service.UserService;
 import com.freeder.buclserver.core.security.CustomUserDetails;
+import com.freeder.buclserver.domain.user.dto.response.UserAffiliateResponse;
 import com.freeder.buclserver.domain.user.dto.response.UserProfileResponse;
 import com.freeder.buclserver.global.response.BaseResponse;
 
@@ -21,11 +25,19 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
 
 	private final UserService userService;
+	private final AffiliateService affiliateService;
 
 	@GetMapping("/v1/my/profile")
 	public BaseResponse getMyProfile(@AuthenticationPrincipal CustomUserDetails userDetails) {
 		String userId = userDetails.getUserId();
 		UserProfileResponse userProfile = userService.getMyProfile(Long.valueOf(userId));
 		return new BaseResponse(userProfile, HttpStatus.OK, "요청 성공");
+	}
+
+	@GetMapping("/v1/my/profile/affiliates")
+	public BaseResponse getMyAffiliates(@AuthenticationPrincipal CustomUserDetails userDetails) {
+		String userId = userDetails.getUserId();
+		List<UserAffiliateResponse> userAffiliateList = affiliateService.getMyAffiliates(Long.valueOf(userId));
+		return new BaseResponse(userAffiliateList, HttpStatus.OK, "요청 성공");
 	}
 }
