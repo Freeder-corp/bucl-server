12574f11a121b7e209c36044eda139565886b1fe
sujeong11 <qalzm7351@gmail.com>
2023-12-01 07:55:18 +0900

    feat: 자신의 주문 내역 전체 조회 API 구현

--- src/main/java/com/freeder/buclserver/app/orders/OrdersService.java
+++ src/main/java/com/freeder/buclserver/app/orders/OrdersService.java
@@ -1,4 +1,41 @@
 package com.freeder.buclserver.app.orders;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.freeder.buclserver.domain.consumerorder.entity.ConsumerOrder;
+import com.freeder.buclserver.domain.consumerorder.repository.ConsumerOrderRepository;
+import com.freeder.buclserver.domain.consumerpurchaseorder.repository.ConsumerPurchaseOrderRepository;
+import com.freeder.buclserver.domain.shipping.entity.Shipping;
+import com.freeder.buclserver.domain.shipping.repository.ShippingRepository;
+import com.freeder.buclserver.domain.user.dto.response.UserOrderResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
 public class OrdersService {
+
+	private final ConsumerOrderRepository consumerOrderRepository;
+	private final ConsumerPurchaseOrderRepository consumerPurchaseOrderRepository;
+	private final ShippingRepository shippingRepository;
+
+	public List<UserOrderResponse> getMyOrders(Long userId) {
+		List<UserOrderResponse> orderResponseList = new ArrayList<>();
+
+		List<ConsumerOrder> consumerOrderList =
+			consumerOrderRepository.findAllByConsumer_IdOrderByCreatedAtDesc(userId);
+
+		for (ConsumerOrder consumerOrder : consumerOrderList) {
+			// TODO: 1개의 주문에 대해 한 옵션만 선택 가능함에 따라 로직 변경 필요
+			int totalProductQty = consumerPurchaseOrderRepository.findTotalProductOrderQty(consumerOrder.getId());
+			Shipping shipping = shippingRepository.findByConsumerOrder_Id(consumerOrder.getId());
+			UserOrderResponse productResponse = UserOrderResponse.from(consumerOrder, totalProductQty, shipping);
+			orderResponseList.add(productResponse);
+		}
+
+		return orderResponseList;
+	}
 }
