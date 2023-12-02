1be92eb696ed2c81456ef56cf375c3fdff2d819a
sujeong11 <qalzm7351@gmail.com>
2023-12-02 15:56:35 +0900

    fix: 프로필 관련 쿼리메서드 최적화
    - join 쿼리가 나가지 않도록 엔티티로 연관관계 조회
    - 프로필 관련 데이터 조회 전 해당 회원이 활성화된 회원인지 확인

--- src/main/java/com/freeder/buclserver/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/freeder/buclserver/domain/payment/repository/PaymentRepository.java
@@ -2,9 +2,10 @@ package com.freeder.buclserver.domain.payment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.freeder.buclserver.domain.consumerorder.entity.ConsumerOrder;
 import com.freeder.buclserver.domain.payment.entity.Payment;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
-	Payment findByConsumerOrder_Id(Long consumerOrderId);
+	Payment findByConsumerOrder(ConsumerOrder consumerOrder);
 }
