cfa5578fadf84b1f3e44c965ba9101eb7891c79d
sujeong11 <qalzm7351@gmail.com>
2023-12-10 02:13:00 +0900

    feat: 회원 배송지 수정 API 구현

---
+++ src/main/java/com/freeder/buclserver/domain/usershippingaddress/dto/request/AddressUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.freeder.buclserver.domain.usershippingaddress.dto.request;
+
+import javax.validation.constraints.NotBlank;
+
+public record AddressUpdateRequest(
+	@NotBlank String shippingAddressName,
+	@NotBlank String recipientName,
+	@NotBlank String zipCode,
+	@NotBlank String address,
+	@NotBlank String addressDetail,
+	@NotBlank String contactNumber,
+	boolean isDefaultAddress
+) {
+}
