b33e02156e9ff57b56beb5dc7acf79403e871dee
sujeong11 <qalzm7351@gmail.com>
2024-01-10 08:20:51 +0900

    feat: 500 에러 발생 시 discord에 알림 보내는 기능
    = 알림에는 에러에 대한 클래스 이름, 발생 위치, 메시지가 포함

--- src/main/java/com/freeder/buclserver/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/freeder/buclserver/global/exception/GlobalExceptionHandler.java
@@ -1,16 +1,94 @@
 package com.freeder.buclserver.global.exception;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.freeder.buclserver.global.exception.util.DiscordAlarmUtil;
 import com.freeder.buclserver.global.response.ErrorResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
 	@ExceptionHandler(BaseException.class)
-	public ResponseEntity<?> handleRestApiException(BaseException error) {
-		return new ResponseEntity<>(new ErrorResponse(error.getHttpStatus(), error.getErrorMessage()),
-			error.getHttpStatus());
+	public ResponseEntity<ErrorResponse> handleRestApiException(BaseException ex) {
+		String stackTrace = convertExceptionStackTraceToString(ex);
+		log.error("[Base_Exception]: {}", stackTrace);
+
+		if (ex.getHttpStatus() == HttpStatus.INTERNAL_SERVER_ERROR) {
+			DiscordAlarmUtil.send(ex, ex.getErrorMessage(), stackTrace);
+		}
+
+		return ResponseEntity
+			.status(ex.getHttpStatus())
+			.body(new ErrorResponse(ex.getHttpStatus(), ex.getErrorMessage()));
+	}
+
+	@ExceptionHandler(ConstraintViolationException.class)
+	public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException ex) {
+		log.error("[Constraint_Violation_Exception]: {}", convertExceptionStackTraceToString(ex));
+
+		return ResponseEntity
+			.status(HttpStatus.BAD_REQUEST)
+			.body(new ErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage()));
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+		MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request
+	) {
+		log.error("[MethodArgument_NotValid_Exception]: {}", convertExceptionStackTraceToString(ex));
+
+		return ResponseEntity
+			.status(status)
+			.body(new ErrorResponse(status, ex.getMessage()));
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(
+		Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request
+	) {
+		String stackTrace = convertExceptionStackTraceToString(ex);
+		log.error("[Spring_MVC_Standard_Exception]: {}", stackTrace);
+
+		if (status == HttpStatus.INTERNAL_SERVER_ERROR) {
+			DiscordAlarmUtil.send(ex, ex.getMessage(), stackTrace);
+		}
+
+		return ResponseEntity
+			.status(status)
+			.body(new ErrorResponse(status, ex.getMessage()));
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+		String stackTrace = convertExceptionStackTraceToString(ex);
+		log.error("[Exception]: {}", stackTrace);
+
+		DiscordAlarmUtil.send(ex, ex.getMessage(), stackTrace);
+
+		return ResponseEntity
+			.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			.body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage()));
+	}
+
+	private static String convertExceptionStackTraceToString(Exception ex) {
+		StringWriter stringWriter = new StringWriter();
+		ex.printStackTrace(new PrintWriter(stringWriter));
+		return String.valueOf(stringWriter);
 	}
 }
---
+++ src/main/java/com/freeder/buclserver/global/exception/util/DiscordAlarmUtil.java
@@ -0,0 +1,61 @@
+package com.freeder.buclserver.global.exception.util;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class DiscordAlarmUtil {
+
+	private static String webhookUrl;
+
+	@Value("${discord.webhook.url}")
+	private void setWebhookUrl(String url) {
+		webhookUrl = url;
+	}
+
+	public static void send(Exception ex, String errorMessage, String stackTrace) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+
+		try {
+			new RestTemplate().exchange(
+				webhookUrl,
+				HttpMethod.POST,
+				new HttpEntity<>(createMessage(ex, errorMessage, stackTrace), headers),
+				String.class
+			);
+		} catch (Exception e) {
+			log.error("디스코드에 500 에러에 대한 알림을 보내는데 실패했습니다.");
+		}
+	}
+
+	public static String createMessage(Exception ex, String errorMessage, String stackTrace) {
+		JSONObject jsonMessage = new JSONObject();
+		String errorLocation = stackTrace.split("\\n")[1].trim();
+
+		try {
+			jsonMessage.put("content", "\uD83D\uDD25  500 에러가 발생하였습니다");
+			JSONObject embeds = new JSONObject()
+				.put("title", ex.getClass() + " " + errorLocation)
+				.put("description", errorMessage);
+
+			JSONArray embedsArray = new JSONArray().put(embeds);
+
+			jsonMessage.put("embeds", embedsArray);
+		} catch (JSONException e) {
+			log.error("디스코드에 보낼 내용을 만드는 과정에서 에러가 발생했습니다.");
+		}
+		return jsonMessage.toString();
+	}
+}
