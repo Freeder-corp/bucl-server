db0fa412ff57ea8a8bcaa95d08b163754d9de2d3
Changmin Kim <ckd29672@naver.com>
2024-02-14 13:17:20 +0900

    fix: 금융결제원 관련 api 수정
    - 나중에 한번 봐야 될듯

--- src/main/java/com/freeder/buclserver/app/rewards/RewardsAccountController.java
+++ src/main/java/com/freeder/buclserver/app/rewards/RewardsAccountController.java
@@ -1,14 +1,15 @@
 package com.freeder.buclserver.app.rewards;
 
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.freeder.buclserver.app.rewards.dto.PostAccountRealNameReq;
+import com.freeder.buclserver.core.security.CustomUserDetails;
 import com.freeder.buclserver.domain.openbanking.dto.OpenBankingAccessTokenDto;
 import com.freeder.buclserver.domain.rewardwithdrawalaccount.dto.WithdrawalAccountResponseDto;
 import com.freeder.buclserver.global.exception.BaseException;
@@ -28,21 +29,18 @@ public class RewardsAccountController {
 
 	@PostMapping("/token")
 	public BaseResponse<OpenBankingAccessTokenDto> requestOpenApiAccessToken() {
-		try {
-			OpenBankingAccessTokenDto accessToken = openBankingService.requestOpenApiAccessToken();
-			return new BaseResponse<>(accessToken, HttpStatus.OK, "Success");
-		} catch (Exception e) {
-			throw new BaseException(HttpStatus.INTERNAL_SERVER_ERROR, 500, e.getMessage());
-		}
+		OpenBankingAccessTokenDto accessToken = openBankingService.requestOpenApiAccessToken();
+		return new BaseResponse<>(accessToken, HttpStatus.OK, "Success");
 	}
 
 	@PostMapping("/realname")
-	public BaseResponse<Boolean> requestMatchAccountRealName(@RequestBody Map<String, Object> requestBody) {
-		Long userId = ((Number)requestBody.get("userId")).longValue();
-		String bankCode = (String)requestBody.get("bankCode");
-		String bankAccount = (String)requestBody.get("bankAccount");
-		String realName = (String)requestBody.get("realName");
-		String birthday = (String)requestBody.get("birthday");
+	public BaseResponse<Boolean> requestMatchAccountRealName(@RequestBody PostAccountRealNameReq requestBody,
+		@AuthenticationPrincipal CustomUserDetails userDetails) {
+		Long userId = Long.valueOf(userDetails.getUserId());
+		String bankCode = requestBody.getBankCode();
+		String bankAccount = requestBody.getBankAccount();
+		String realName = requestBody.getRealName();
+		String birthday = requestBody.getBirthday();
 
 		try {
 			boolean result = rewardsWithdrawalAccountService.requestMatchAccountRealName(userId, bankCode, bankAccount,
@@ -55,20 +53,16 @@ public class RewardsAccountController {
 	}
 
 	@GetMapping("/account")
-	public BaseResponse<WithdrawalAccountResponseDto> responseWithdrawalAccount() {
-		try {
-			Long userId = 11L;
-			WithdrawalAccountResponseDto responseDto = rewardsWithdrawalAccountService.getWithdrawalAccountByUserId(
-				userId);
+	public BaseResponse<WithdrawalAccountResponseDto> responseWithdrawalAccount(
+		@AuthenticationPrincipal CustomUserDetails userDetails) {
+		Long userId = Long.valueOf(userDetails.getUserId());
+		WithdrawalAccountResponseDto responseDto = rewardsWithdrawalAccountService.getWithdrawalAccountByUserId(
+			userId);
 
-			if (responseDto != null) {
-				return new BaseResponse<>(responseDto, HttpStatus.OK, "Success");
-			} else {
-				return new BaseResponse<>(null, HttpStatus.BAD_REQUEST, "에러가 발생했습니다.");
-			}
-		} catch (Exception e) {
-			throw new BaseException(HttpStatus.INTERNAL_SERVER_ERROR, 500, e.getMessage());
+		if (responseDto != null) {
+			return new BaseResponse<>(responseDto, HttpStatus.OK, "Success");
+		} else {
+			return new BaseResponse<>(null, HttpStatus.BAD_REQUEST, "에러가 발생했습니다.");
 		}
 	}
-
 }
