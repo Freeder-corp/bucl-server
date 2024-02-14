ae9457a46a45f1ca411f099d1018674bbf202a72
Changmin Kim <ckd29672@naver.com>
2024-02-14 14:16:20 +0900

    fix: user repository findBySocialId 메소드 주석 처리

--- src/main/java/com/freeder/buclserver/domain/user/repository/UserRepository.java
+++ src/main/java/com/freeder/buclserver/domain/user/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.freeder.buclserver.domain.user.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +8,5 @@ import com.freeder.buclserver.domain.user.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	Optional<User> findBySocialId(String socialId);
+	// Optional<User> findBySocialId(String socialId);
 }
